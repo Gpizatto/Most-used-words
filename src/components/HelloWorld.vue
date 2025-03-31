7786732de005fde4544d6393ca45ca509bfdf1ec
GUSTAVO PIZATTO <gustavopizatto@hotmail.com>
2025-03-31 15:32:28 -0300

    Home.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,52 @@
 <template>
-  <div> olá</div>
+<div class="home"> 
+    <v-form>
+        <v-file-input label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-icon="mdi-send"
+        outlided
+        multiple
+        v-model="files"
+        @click:append="processSubtitles"
+        >
+    </v-file-input>
+    </v-form>
+
+     <div class="pills">
+            <PillComponent v-for="item in groupedWords" :word="item.word"
+            :amount="item.amount" :key="item.word"/>
+        
+         
+        </div>
+    </div>
 </template>
 
 <script>
+import PillComponent from './PillComponent';
+const {IpcRenderer} =window.require('electron')
 export default {
     name:'HomeComponent',
+    components:{ PillComponent},
+    data:function(){
+        return{
+            files:[],
+            groupedWords: [
+            {word:'i',amount:547},
+                {word: 'it', amount:300},            
+                {word: 'do', amount:100}, {word:'i',amount:547},
+            
+              ]      
+        }
+    },
+    methods:{
+        processSubtitles(){
+            IpcRenderer.send("blabla", "alo")
+            console.log(this.files)
+
+        }
+    }
+
+    
     
 
         
@@ -12,5 +54,15 @@ export default {
 </script>
 
 <style>
+.home{
+    margin: 20px;
+}
 
-</style>
+
+.pills{
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:center;
+
+}
+</style >
